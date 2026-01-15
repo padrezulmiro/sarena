7f80d02dca04761fdcc84bebd6de51d902cbb4c9
aZul <fmdcosta.piano@gmail.com>
2026-01-15 11:33:17 +0000

    wip: Sync 14/1/26

--- src/ai.ts
+++ src/ai.ts
@@ -1,8 +1,10 @@
+import { RESOURCE_ENERGY } from "game/constants"
 import type { Creep, GameObject } from "game/prototypes"
 
 export type AI = {
     blackboard: Record<string, any>
-    run(target: GameObject): void
+    bTrees?: Record<>
+    run(agent: GameObject): void
 }
 
 export const minerAI: AI = {
@@ -10,7 +12,15 @@ export const minerAI: AI = {
         state: "harvest"
     },
 
-    run(target: Creep) {
+    run(agent: Creep) {
+        if (this.blackboard["state"] == "harvest" &&
+            agent.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+            this.blackboard["state"] = "deposit"
+        } else if (this.blackboard["state"] == "deposit" &&
+            agent.store.getUsedCapacity(RESOURCE_ENERGY) == 0) {
+            this.blackboard["state"] = "harvest"
+        }
 
+        // TODO Call behaviour trees
     }
 }
